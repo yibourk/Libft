beba53336d4a62032a3a535a33c3199631a02470
Younes <ibourk.younes@hotmail.com>
2024-11-07 17:53:04 +0100

    resolved git issues/changes

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-La programmation en C est une activité très laborieuse dès lors que l’on n’a pas accès
-à toutes ces petites fonctions usuelles très pratiques. C’est pourquoi nous vous proposons
-à travers ce projet de prendre le temps de récrire ces fonctions, de les comprendre et
-de vous les approprier. Vous pourrez alors réutiliser votre bibliothèque pour travailler
-efficacement sur vos projets suivants en C.
+# Libft
+
+## La programmation en C est une activité très laborieuse dès lors que l’on n’a pas accès à toutes ces petites fonctions usuelles très pratiques. C’est pourquoi nous vous proposons à travers ce projet de prendre le temps de récrire ces fonctions, de les comprendre et de vous les approprier. Vous pourrez alors réutiliser votre bibliothèque pour travailler efficacement sur vos projets suivants en C.
--- libft.h
+++ libft.h
@@ -18,7 +18,7 @@
 
 int		ft_atoi(const char *nptr);
 void	ft_bzero(void *s, size_t n);
-void	*calloc(size_t nmemb, size_t size);
+void	*ft_calloc(size_t nmemb, size_t size);
 
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
@@ -33,12 +33,12 @@ void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memset(void *s, int c, size_t n);
 
 char	*ft_strchr(const char *s, int c);
-char	*strdup(const char *s);
+char	*ft_strdup(const char *s);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlen(char *str);
-int		strncmp(const char *s1, const char *s2, size_t n);
-char	*strrchr(const char *s, int c);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+char	*ft_strrchr(const char *s, int c);
 
 int		ft_toupper(int c);
 int		ft_tolower(int c);
