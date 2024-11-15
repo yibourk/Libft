006c8d148a9a294493f25ca2aced7cf2db27bff1
Younes Ibourk <yibourk@c3r4p9.42perpignan.fr>
2024-11-15 14:17:49 +0100

    ft_strnstr.c added

---
+++ ft_strnstr.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yibourk <yibourk@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/15 14:16:47 by yibourk           #+#    #+#             */
+/*   Updated: 2024/11/15 14:16:52 by yibourk          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t	i;
+	size_t	j;
+	char	*result;
+
+	i = 0;
+	result = (char *)haystack;
+	if (*needle == 0)
+		return (result);
+	while (haystack[i] && i < len)
+	{
+		j = 0;
+		while (needle[j] && needle[j] == haystack[i + j] && i + j < len)
+		{
+			if (needle[j + 1] == 0)
+				return ((char *)(result + i));
+			j++;
+		}
+		i++;
+	}
+	return (NULL);
+}
